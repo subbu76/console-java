8046556e7aa5b09063d370ad634b4970a9562b7c
Subbu <mesubbu@outlook.com>
2017-09-25 21:53:20 -0400

    pom.xml

---
+++ src/main/java/poc/subbu/App.java
@@ -0,0 +1,13 @@
+package poc.subbu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/poc/subbu/StringUtils.java
@@ -0,0 +1,17 @@
+package poc.subbu;
+
+public class StringUtils {
+
+	public static boolean isPalindrome(String input) {
+		boolean is_Pali = false;
+		if (null!=input && !input.trim().equals("")) {
+			StringBuilder reverse = new StringBuilder("");
+			for (int i=input.length()-1;i>=0;i--) {
+				reverse.append(input.charAt(i));
+			}
+			is_Pali = reverse.toString().equalsIgnoreCase(input);
+		}
+		return is_Pali;
+	}
+	
+}
